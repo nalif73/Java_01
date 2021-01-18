01cac1263c36a2a6e19ab9c79890a786a8d50d62
Nalif73 <nalif73@gmail.com>
2021-01-18 22:19:37 +0300

    lesson-06 final

--- src/com/nalif73/lesson06/Cat.java
+++ src/com/nalif73/lesson06/Cat.java
@@ -1,5 +1,17 @@
 package com.nalif73.lesson06;
 
 public class Cat extends Animals {
-
+    public Cat(String name) {
+        super(name, 200, 0);
+    }
+    @Override
+    public  void run(int dist) {
+        if (dist<=getMaxRun())
+            System.out.println(getName()+ " пробежал " + dist + " м");
+        else System.out.println(getName() +" так далеко не может бегать. Максимум " + getMaxRun() + " м");
+    }
+    @Override
+    public  void swim(int dist) {
+        System.out.println(getName() + " не умеет плавать");
+    }
 }
